67f58b0587241ce0ccca05fcb1d68037bf12a097
Shubham Kirve <srkirve77@gmail.com>
2020-10-23 03:05:21 +0530

    Delete receiveMessage.java

--- src/main/java/com/geektrust/tameofthrones/service/receiveMessage.java
+++
@@ -1,8 +0,0 @@
-package com.geektrust.tameofthrones.service;
-
-import com.geektrust.tameofthrones.dto.Kingdom;
-import com.geektrust.tameofthrones.exchanges.GetMessage;
-
-public interface receiveMessage{
-    public boolean decipherMessage(GetMessage getMessage);
-}
