053a0e5bf871b10e4515f34f3306a91c8fd4ca1a
Shubham Kirve <srkirve77@gmail.com>
2020-10-23 03:05:43 +0530

    Delete stringOperations.java

--- src/main/java/com/geektrust/tameofthrones/service/stringOperations.java
+++
@@ -1,8 +0,0 @@
-package com.geektrust.tameofthrones.service;
-
-
-public interface stringOperations{    
-    public  String sortString(String inputString);
-    public  String rotateString(String message, int noOftimes);
-    public  boolean verifyStringPresence(String rotatedString, String allyEmblem);
-}
